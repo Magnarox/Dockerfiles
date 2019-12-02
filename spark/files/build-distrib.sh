01cd9e5855a54e5a6c0f3f1f3b4ba1e23ac6e4bc
Magnarox <magnarox06@gmail.com>
2019-12-02 07:57:48 +0100

    FIX qpdf need to check Doc

--- spark/files/build-distrib.sh
+++ spark/files/build-distrib.sh
@@ -10,4 +10,4 @@ cd /spark/spark-2.4.4/
 export MAVEN_OPTS="-Xmx2g -XX:ReservedCodeCacheSize=512m"
 ./dev/make-distribution.sh --name magnarox-spark --pip --r --tgz -Psparkr -Phadoop-3.1 -Dhadoop.version=3.1.2 -Phive -Phive-thriftserver -Pkubernetes
 
-cp dist/*.tgz ../out/
+cp *.tgz ../out/
